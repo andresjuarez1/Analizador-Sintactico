7d1358337e67eff37406aac94a25701e275d5e64
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2024-02-15 14:18:45 -0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,66 +2,53 @@ import { useState } from 'react';
 import MonacoEditor from 'react-monaco-editor';
 
 const tiposToken = [
-  { regex: /^=/, token: "Igual" },
+  { regex: /^num/, token: "PalabraReservadaNum" },
+  { regex: /^string/, token: "PalabraReservadaString" },
+  { regex: /^float/, token: "PalabraReservadaFloat" },
+  { regex: /^function/, token: "PalabraReservadaFunction" },
+  { regex: /^[_a-zA-Z][_a-zA-Z0-9]*/, token: "Identificador" },
+  { regex: /^"[^"]*"/, token: "Cadena" },
+  { regex: /^-?[0-9]+(?:\.[0-9]+)?/, token: "Numero" },
+  { regex: /^=/, token: "Asignacion" },
   { regex: /^;/, token: "PuntoYComa" },
-  { regex: /^_/, token: "GuionBajo" },
   { regex: /^{/, token: "LlaveApertura" },
-  { regex: /^}/, token: "LlaveFinal" },
-  { regex: /^\[/, token: "CorcheteApertura" },
-  { regex: /^\]/, token: "CorcheteCierre" },
-  { regex: /^\"/, token: "ComillasDobles" },
+  { regex: /^}/, token: "LlaveCierre" },
   { regex: /^\(/, token: "ParentesisApertura" },
   { regex: /^\)/, token: "ParentesisCierre" },
-  { regex: /^\d+/, token: "Digitos" },
-  { regex: /^function/, token: "PalabraReservadaFunction" },
-  { regex: /^for/, token: "PalabraReservadaFor" },
+  { regex: /^==/, token: "Igualdad" },
   { regex: /^if/, token: "PalabraReservadaIf" },
   { regex: /^else/, token: "PalabraReservadaElse" },
   { regex: /^while/, token: "PalabraReservadaWhile" },
-  { regex: /^num/, token: "PalabraReservadaNum" },
-  { regex: /^float/, token: "PalabraReservadaFloat" },
-  { regex: /^string/, token: "PalabraReservadaString" },
-  { regex: /^\+\+/, token: "Incremento" },
-  { regex: /^--/, token: "Decremento" },
-  { regex: /^>/, token: "OperadorMayor" },
-  { regex: /^</, token: "OperadorMenor" },
-  { regex: /^>=/, token: "OperadorIgualMayor" },
-  { regex: /^<=/, token: "OperadorIgualMenor" },
-  { regex: /^==/, token: "OperadorIgualdad" },
-  { regex: /^[a-zA-Z]+/, token: "Letras" },
+  { regex: /^for/, token: "PalabraReservadaFor" },
 ];
 
-// Función para realizar el análisis léxico
 function lex(input) {
   let tokens = [];
-  let position = 0;  // Seguimiento de la posición para mensajes de error más informativos.
+  let position = 0;
 
   while (input.length > 0) {
-    // Ignora espacios y saltos de línea al principio de la cadena de entrada.
     const whitespace = input.match(/^\s+/);
     if (whitespace) {
-      position += whitespace[0].length;  // Actualiza la posición.
-      input = input.slice(whitespace[0].length);  // Elimina los espacios de la entrada.
+      position += whitespace[0].length;
+      input = input.slice(whitespace[0].length);
     }
 
-    if(input.length === 0) {
-      break;  // Si solo quedan espacios en blanco, termina el bucle.
+    if (input.length === 0) {
+      break;
     }
 
     let match = false;
     for (let tokenType of tiposToken) {
       const result = tokenType.regex.exec(input);
       if (result !== null) {
-        // Se ha encontrado un token
         match = true;
         tokens.push({ type: tokenType.token, value: result[0] });
-        position += result[0].length; 
+        position += result[0].length;
         input = input.slice(result[0].length);
         break;
       }
     }
 
-    // Si no se encuentra ningún token, se agrega un token de error.
     if (!match) {
       const errorToken = input[0];
       tokens.push({ type: "Error", value: `Carácter inesperado '${errorToken}' en la posición ${position}.` });
@@ -72,24 +59,113 @@ function lex(input) {
   return tokens;
 }
 
+function parse(tokens) {
+  let currentPosition = 0;
+
+  function nextToken() {
+    currentPosition++;
+    return tokens[currentPosition];
+  }
+
+  function expect(tokenType) {
+    if (tokens[currentPosition].type === tokenType) {
+      return nextToken();
+    } else {
+      throw new Error(`Error de sintaxis: Se esperaba ${tokenType} pero se encontró ${tokens[currentPosition].type}`);
+    }
+  }
+
+  function parseVariableDeclaration() {
+    if (
+      tokens[currentPosition].type === 'PalabraReservadaNum' ||
+      tokens[currentPosition].type === 'PalabraReservadaFloat' ||
+      tokens[currentPosition].type === 'PalabraReservadaString'
+    ) {
+      const tipoVariable = tokens[currentPosition].type; // Guardar el tipo de variable
+
+      nextToken();
+      expect('Identificador');
+      if (tokens[currentPosition].type === 'Asignacion') {
+        nextToken();
+        if (
+          (tipoVariable === 'PalabraReservadaNum' && tokens[currentPosition].type === 'Numero') ||
+          (tipoVariable === 'PalabraReservadaFloat' && (tokens[currentPosition].type === 'Numero' || tokens[currentPosition].type === 'Identificador')) ||
+          (tipoVariable === 'PalabraReservadaString' && tokens[currentPosition].type === 'Cadena')
+        ) {
+          nextToken();
+        } else {
+          throw new Error(`Error de sintaxis: El tipo de variable '${tipoVariable}' no coincide con el valor asignado '${tokens[currentPosition].type}'`);
+        }
+      }
+      expect('PuntoYComa');
+    } else {
+      throw new Error(`Error de sintaxis: Se esperaba PalabraReservadaNum, PalabraReservadaFloat o PalabraReservadaString pero se encontró ${tokens[currentPosition].type}`);
+    }
+  }
+
+
+
+  function parseElseStatement() {
+    expect('LlaveApertura');
+    expect('LlaveCierre');
+  }
+
+  function parseFunctionDeclaration() {
+    expect('PalabraReservadaFunction');
+    expect('Identificador');
+    expect('ParentesisApertura');
+    expect('ParentesisCierre');
+    expect('LlaveApertura');
+    expect('LlaveCierre');
+  }
+
+  function parseExpression() {
+    expect('Identificador');
+    expect('Igualdad');
+    expect('Identificador');
+  }
+
+  function parseProgram() {
+    while (currentPosition < tokens.length) {
+      const token = tokens[currentPosition];
+      if (token.type === 'PalabraReservadaNum' || token.type === 'PalabraReservadaFloat' || token.type === 'PalabraReservadaString') {
+        parseVariableDeclaration();
+      } else if (token.type === 'PalabraReservadaFunction') {
+        parseFunctionDeclaration();
+      } else {
+        throw new Error(`Error de sintaxis: Token inesperado ${token.type}`);
+      }
+    }
+  }
+
+  parseProgram();
+}
+
 function App() {
   const [codigoFuente, setCodigoFuente] = useState('');
   const [tokensGenerados, setTokensGenerados] = useState([]);
+  const [error, setError] = useState('');
 
-  // Función para analizar el código fuente y generar los tokens
   const analizarCodigo = () => {
     const tokens = lex(codigoFuente);
     setTokensGenerados(tokens);
+    try {
+      parse(tokens);
+      setError('');
+      console.log('Análisis sintáctico exitoso');
+    } catch (error) {
+      setError(error.message);
+      console.error(error.message);
+    }
   };
 
-  // Función ejecutada al montar el editor
   const editorDidMount = (editor, monaco) => {
-    console.log('Editor created');
+    console.log('Editor creado');
   };
 
   return (
     <div className='mx-8'>
-      <h1 className='font-medium text-3xl my-10 text-white'>Analizador Lexico</h1>
+      <h1 className='font-medium text-3xl my-10 text-white'>Analizador Sintáctico</h1>
       <div className='flex'>
         <MonacoEditor
           width="1000"
@@ -107,6 +183,7 @@ function App() {
               <li key={index} className='text-[#EFFD95]'>{`${token.type} (${token.value})`}</li>
             ))}
           </ul>
+          {error && <p className="text-red-500">{error}</p>}
         </div>
       </div>
       <button onClick={analizarCodigo} className='bg-[#DCEF64] h-12 font-medium w-28 rounded-md'>Analizar</button>
